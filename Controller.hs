ae79601f6ef23911aa38bf2bb3ec65171e55c12d
Marcin Gosk <mgosk@mion.elka.pw.edu.pl>
2013-01-13 17:40:43 +0100

    Czemu kurwa nie działa

--- Controller.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE CPP, DeriveDataTypeable, FlexibleContexts, GeneralizedNewtypeDeriving, 
-   MultiParamTypeClasses, TemplateHaskell, TypeFamilies, RecordWildCards #-}
-   
-module Controller where
-import Model
-
-
-addSubject id name = 
--- Planner.hs
+++ Planner.hs
@@ -1,58 +1,85 @@
-{-# LANGUAGE CPP, DeriveDataTypeable, FlexibleContexts, GeneralizedNewtypeDeriving, 
-  MultiParamTypeClasses, TemplateHaskell, TypeFamilies, RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving, RecordWildCards
+  , TemplateHaskell, TypeFamilies, OverloadedStrings #-}
 
 module Main where
-
-import Control.Applicative  ( (<$>) )
-import Control.Exception    ( bracket )
-import Control.Monad        ( msum )
-import Control.Monad.Reader ( ask )
-import Control.Monad.State  ( get, put )
-import Data.Data            ( Data, Typeable )
-import Happstack.Server     ( Response, ServerPart, dir, nullDir, nullConf, ok
-                            , simpleHTTP, toResponse )
-import Data.Acid            ( AcidState, Query, Update, makeAcidic, openLocalState )
-import Data.Acid.Advanced   ( query', update' )
-import Data.Acid.Local      ( createCheckpointAndClose )
-import Data.SafeCopy        ( base, deriveSafeCopy )
+import Control.Applicative  ((<$>), optional)
+import Control.Exception    (bracket)
+import Control.Monad        (msum, mzero)
+import Control.Monad.Reader (ask)
+import Control.Monad.State  (get, put)
+import Control.Monad.Trans  (liftIO)
+import Data.Acid            (AcidState, Update, Query, makeAcidic, openLocalState)
+import Data.Acid.Advanced   (update', query')
+import Data.Acid.Local      (createCheckpointAndClose)
+import Data.Data            (Data, Typeable)
+import Data.IxSet           ( Indexable(..), IxSet(..), (@=), Proxy(..), getOne
+                            , ixFun, ixSet )
+import qualified Data.IxSet as IxSet
+import Data.SafeCopy        (SafeCopy, base, deriveSafeCopy)
+import Data.Text            (Text)
+import Data.Text.Lazy       (toStrict)
+import qualified Data.Text  as Text
+import Data.Time            (UTCTime(..), getCurrentTime)
+import Happstack.Server     ( ServerPart, Method(POST, HEAD, GET), Response, decodeBody
+                            , defaultBodyPolicy, dir, lookRead, lookText, method
+                            , notFound, nullConf, nullDir, ok, seeOther, simpleHTTP
+                            , toResponse)
+import qualified Text.Blaze.Html4.Strict as H
+import qualified Text.Blaze.Html4.Strict.Attributes as A
 import SubjectView
-import Controller
 
+newtype SubjectId = SubjectId { unSubjectId :: Integer }
+    deriving (Eq, Ord, Data, Enum, Typeable, SafeCopy)
+newtype SubjectName     = SubjectName Text    deriving (Eq, Ord, Data, Typeable, SafeCopy)
 
-data CounterState = CounterState { count :: Integer }
-    deriving (Eq, Ord, Read, Show, Data, Typeable)
+data Subject = Subject
+    { subjectID  :: SubjectId
+    , subjectName  :: Text
+    }
+    deriving (Eq, Ord, Data, Typeable)
 
-$(deriveSafeCopy 0 'base ''CounterState)
+$(deriveSafeCopy 0 'base ''Subject)
+instance Indexable Subject where
+    empty = ixSet [ ixFun $ \bp -> [ subjectID bp ]
+                  , ixFun $ \bp -> [ SubjectName  $ subjectName bp  ]
+                  ]
+data PlannerState = PlannerState
+    { nextSubjectId :: SubjectId
+    , subjects      :: IxSet Subject
+    }
+    deriving (Eq, Ord,Data, Typeable)
 
-initialCounterState :: CounterState
-initialCounterState = CounterState 0
-incCountBy :: Integer -> Update CounterState Integer
-incCountBy n =
-    do c@CounterState{..} <- get
-       let newCount = count + n
-       put $ c { count = newCount }
-       return newCount
+$(deriveSafeCopy 0 'base ''PlannerState)
 
-peekCount :: Query CounterState Integer
-peekCount = count <$> ask
+initialPlannerState :: PlannerState
+initialPlannerState =
+            PlannerState { nextSubjectId = SubjectId 1
+                       , subjects      = empty
+                       }
 
-$(makeAcidic ''CounterState ['incCountBy, 'peekCount])
+-- | create a new, empty post and add it to the database
+newSubject :: Text -> Update PlannerState Subject
+newSubject subjectName =
+    do b@PlannerState{..} <- get
+       let subject = Subject { subjectID = nextSubjectId
+                       , subjectName  = Text.empty
+                                 }
+       put $ b { nextSubjectId = succ nextSubjectId
+               , subjects      = IxSet.insert subject subjects
+               }
+       return subject
 
 main :: IO ()
 main =
-    do bracket (openLocalState initialCounterState)
+    do bracket (openLocalState initialPlannerState)
                (createCheckpointAndClose)
                (\dispatcher ->
                     simpleHTTP nullConf (handlers dispatcher))
 
-handlers :: AcidState CounterState -> ServerPart Response
+handlers :: AcidState PlannerState -> ServerPart Response
 handlers dispatcher =
-    msum [ dir "peek" $ do c <- query' dispatcher PeekCount
-                           ok $ toResponse $ "peeked at the count and saw: " ++ show c
-         , dir "subject" $ subjectDispatcher
-	 , do nullDir
-              c <- update' dispatcher (IncCountBy 1)
-              ok $ toResponse $ "New count is: " ++ show c
-
+    msum [ dir "subject" $ subjectDispatcher
+    	 , do nullDir
+	      ok $ toResponse $ "New count is: "
          ]
 
